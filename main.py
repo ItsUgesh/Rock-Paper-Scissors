21d57b1e5c1a6bede56bc97e70cf48bd6eb31896
ugesh01 <102455256+ItsUgesh@users.noreply.github.com>
2025-09-24 09:45:10 +0545

    Emote Added

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ print("Player choice:", compdict[choice])
 print("Computer choice:", compdict[computer])
 
 if computer == choice:
-    print("Its Draw! Play again")
+    print("Its Draw!👏  Play again")
 else:
     if computer == 1 and choice == 0:
-        print("You Win!")
+        print("You Win!😃👏 ")
     elif computer == 1 and choice == -1:
-        print("You Lose!")
+        print("You Lose!😔")
     elif computer == 0 and choice == 1:
-        print("You Lose!")
+        print("You Lose!😔")
     elif computer == 0 and choice == -1:
-        print("You Win!")
+        print("You Win!😃👏 ")
     elif computer == -1 and choice == 1:
-        print("You Wim!")
+        print("You Wim!😃👏 ")
     elif computer == -1 and choice == 0:
-        print("You Lose!")
+        print("You Lose!😔")
     else:
         print("Somrthing went wrong or user choosed wrong Alphabet")
